f9914cf200a3835a0662691e44f5bbd2b764e559
Sam Cowley <samuel.alexander.cowley@gmail.com>
2021-05-31 23:27:11 -0400

    Initial

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=scowley-ch3-rds
+export POSTGRES_HOST=scowley-ch3-rds.cmmwfio3stmb.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=scowley-ch3-s3
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=hello
+export URL=http://localhost:8101
