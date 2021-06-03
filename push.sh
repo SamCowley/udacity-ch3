017e5eaae70f7fb15dcfdc814f83d833039abc98
Sam Cowley <samuel.alexander.cowley@gmail.com>
2021-06-02 22:54:05 -0400

    Rebuild &all

--- push.sh
+++ push.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-for file in built/*; do
+pushd built &> /dev/null
+for file in *; do
     echo "=========================================="
     echo "Pushing: $(basename $file)"
     echo "=========================================="
     docker push samcowley/$file
 done
+popd
